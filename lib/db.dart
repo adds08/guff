d3067e27c935d0d3d748a311fe51c855f8fb3c66
adds08 <atishshakya@gmail.com>
2025-09-25 22:24:24 +0545

    #

--- lib/db.dart
+++ lib/db.dart
@@ -2,5 +2,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pocketbase/pocketbase.dart';
 
 final pocketbaseProvider = Provider<PocketBase>((ref) {
-  return PocketBase('http://142.93.211.137');
+  return PocketBase('https://guffapi.codebigya.com');
 });
