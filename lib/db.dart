99f76b94bb84ad428a3e4881d0eda2b7455294c4
adds08 <atishshakya@gmail.com>
2025-09-26 00:22:34 +0545

    #

--- lib/db.dart
+++ lib/db.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pocketbase/pocketbase.dart';
 
-const String envValue = String.fromEnvironment("env", defaultValue: "http://127.0.0.1:8090");
+const String envValue = String.fromEnvironment("env", defaultValue: "https://guffapi.codebigya.com");
 
 final pocketbaseProvider = Provider<PocketBase>((ref) {
   PocketBase pocketBase = PocketBase(envValue);
